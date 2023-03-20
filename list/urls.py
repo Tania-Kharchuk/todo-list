40e934a29defab48373347060cc91a7e9217afc8
Tania Kharchuk <tkharchuk14@gmail.com>
2023-03-20 14:15:29 +0200

    Add path to home page, task create, udpate, delete, change ststud and tag list, create, update, delete

---
+++ list/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+
+from list.views import TaskListView, TaskCreateView, TagListView, TagCreateView, TagUpdateView, TagDeleteView, \
+    TaskDeleteView, TaskUpdateView, change_task_status
+
+urlpatterns = [
+    path("", TaskListView.as_view(), name="index"),
+    path("tasks/create", TaskCreateView.as_view(), name="task-create"),
+    path("tasks/change-status", change_task_status, name="change-status"),
+    path("tasks/<int:pk>/delete", TaskDeleteView.as_view(), name="task-delete"),
+    path("tasks/<int:pk>/update", TaskUpdateView.as_view(), name="task-update"),
+    path("tags/", TagListView.as_view(), name="tag-list"),
+    path("tags/create", TagCreateView.as_view(), name="tag-create"),
+    path("tags/<int:pk>/update", TagUpdateView.as_view(), name="tag-update"),
+    path("tags/<int:pk>/delete", TagDeleteView.as_view(), name="tag-delete"),
+    ]
+
+app_name = "list"
